cf88279d85e40fcd91750f2aac6acecd01637868
Balla Kavya <ballakavya594@gmail.com>
2024-05-21 09:59:59 +0530

    Update | db-csv.py

---
+++ db-csv.py
@@ -0,0 +1,22 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM CUTM")
+
+rows = cursor.fetchall()
+
+csv_filename = 'cutm_data.csv'
+
+with open(csv_filename, 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    
+    csv_writer.writerow([i[0] for i in cursor.description])
+    
+    csv_writer.writerows(rows)
+
+conn.close()
+
+print(f"Data has been exported to {csv_filename}")
